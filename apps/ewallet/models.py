1bfce091ecdf69897d8c7edcbaebcc2e3dce2e27
Santosh Purbey <santosh.purbey@logpoint.com>
2019-02-13 08:35:09 +0545

    create profile, change timezone, register profile in admin

--- apps/ewallet/models.py
+++ apps/ewallet/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    MALE = 'm'
+    FEMALE = 'f'
+    GENDER_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+    )
+    profile_pic = models.ImageField(upload_to='profile', null=True, blank=True)
+    address = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+    mobile = models.CharField(max_length=15, unique=True)
+    dob = models.DateField()
+    gender = models.CharField(max_length=1,
+                        choices=GENDER_CHOICES)
+    profession = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.mobile
