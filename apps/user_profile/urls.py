513910ef3852b88bf4d3845e2fc54a8f4a26f5dc
Santosh Purbey <santosh.purbey@logpoint.com>
2019-02-16 22:33:11 +0545

    add default login view and configure settings

--- apps/user_profile/urls.py
+++ apps/user_profile/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+
+from django.contrib.auth.views import LoginView
+
 from .views import signup
 
 urlpatterns = [
-    path('signup/', signup, name="sugnup"),
+    path('signup/', signup, name="signup"),
+    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
 ]
