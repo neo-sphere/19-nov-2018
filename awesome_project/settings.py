72c7168b05ea80a0a72fa2b45052ffcfdd77b036
Santosh Purbey <santosh.purbey@logpoint.com>
2019-02-27 09:14:00 +0545

    add static and media files

--- awesome_project/settings.py
+++ awesome_project/settings.py
@@ -120,8 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_URL = '/static/' # url
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # file location 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 LOGIN_REDIRECT_URL = '/'
--- awesome_project/urls.py
+++ awesome_project/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# main project url
-# 50 apps sabai url yaha rakhda pollutate huncha
-# yesle k  garncha
-# harek app ko aafno aafno url huncha
-# manually create garnicha urls.py file in every apps
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('apps.ewallet.urls')),
     path('', include('apps.user_profile.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
